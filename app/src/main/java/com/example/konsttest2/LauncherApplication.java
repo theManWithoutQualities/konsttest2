23c119833c3c8ca462de680756307cdaeb58618e
Nickolay Konstantinov <konst007@tut.by>
2019-02-25 08:26:25 +0300

    test3: add appmetrica

---
+++ app/src/main/java/com/example/konsttest2/LauncherApplication.java
@@ -0,0 +1,26 @@
+package com.example.konsttest2;
+
+import android.app.Application;
+import android.os.Build;
+
+import com.yandex.metrica.YandexMetrica;
+import com.yandex.metrica.YandexMetricaConfig;
+
+public class LauncherApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // Создание расширенной конфигурации библиотеки.
+        YandexMetricaConfig config = YandexMetricaConfig
+                .newConfigBuilder("4e0e3d84-abb8-4461-b47d-1955998421ad")
+                .withLogs()
+                .build();
+        // Инициализация AppMetrica SDK.
+        YandexMetrica.activate(getApplicationContext(), config);
+        // Отслеживание активности пользователей.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+            YandexMetrica.enableActivityAutoTracking(this);
+        }
+    }
+}
